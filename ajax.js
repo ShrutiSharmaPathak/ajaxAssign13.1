e87692e03c5563bff1d4b8ac6f1e03ead2322212
ShrutiSharmaPathak <ss_shruti@yahoo.com>
2018-10-07 12:10:21 +0530

    first commit

--- ajax.js
+++ ajax.js
@@ -1,13 +1,14 @@
 
- var array=['srting1','srting2','string3','string4','string5'];
+ 
+ var array=['srting1','srting2','string3','string4','string5']; // array declaration
  var promise1=new Promise(function(resolve,reject){
- setTimeout(resolve,(array.length)*1000);
- var arr= array.map(function(e){
+ setTimeout(resolve,(array.length)*1000); // setting the time out  equal to the number of array elements
+ var arr= array.map(function(e){ // converting the array elements to upper case
  return e.toUpperCase();
 });
-resolve(arr);
+resolve(arr); //resolve is used to resolve the promise with a value or result of another promise.	
 });
-promise1.then( x=> console.log(x)
-).catch(function(err){
+promise1.then( x=> console.log(x) // executing on success
+).catch(function(err){  //handling the error
 console.log(err);
 });
